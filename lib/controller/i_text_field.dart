b00266a9bb0bb64744f957e69698d77b6c5d55c2
Duong Quoc Nghi <duongquocnghi2016@gmail.com>
2024-01-09 21:30:11 +0700

    update code

---
+++ lib/controller/i_text_field.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class ITextField extends StatefulWidget {
+  ITextField({Key? key,
+
+  }) : super(key: key){
+
+  }
+
+  @override
+  State<ITextField> createState() => _ITextField();
+}
+
+class _ITextField extends State<ITextField> {
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+
+    );
+  }
+}
